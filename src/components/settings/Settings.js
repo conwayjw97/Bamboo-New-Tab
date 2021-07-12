efb3d10014c10f648bb64504aaa9806f07cd0e96
James <conwayjw97@gmail.com>
2021-07-12 22:54:15 +0100

    Begun work on settings menu

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -3,9 +3,21 @@ import React, { useEffect, useState, useRef } from "react";
 import "./Settings.css";
 
 function Settings(props) {
+  const [showing, setShowing] = useState(false);
+
+  const handleButtonClick = (event) => {
+    setShowing(true);
+  }
+
+  const settingsScreen = () => {
+    if(showing){
+      // https://www.w3schools.com/howto/howto_js_sidenav.asp
+    }
+  }
+
   return (
     <div>
-      <button className="button" style={{top: window.innerHeight-60, left: window.innerWidth-60}}>&#9998;</button>
+      <button className="button" style={{top: window.innerHeight-60, left: window.innerWidth-60}} onClick={handleButtonClick}>&#9998;</button>
     </div>
   );
 }

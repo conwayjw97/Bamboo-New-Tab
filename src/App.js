d32fc9995e0c8daf656f4adc07823754584cf52f
James <conwayjw97@gmail.com>
2021-07-13 22:19:24 +0100

    Settings menu now updating

--- src/App.js
+++ src/App.js
@@ -14,6 +14,21 @@ function App() {
   });
   const [updateCount, setUpdateCount] = useState(0);
 
+  const handleSettingsChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    switch(event.target.id){
+      case "width":
+        setSettings(prev => ({...prev, width: newValue}));
+        break;
+      case "height":
+        setSettings(prev => ({...prev, height: newValue}));
+        break;
+      case "trees":
+        setSettings(prev => ({...prev, trees: newValue}));
+        break;
+    }
+  }
+
   const handleUpdate = () => {
     setUpdateCount(updateCount + 1);
   }
@@ -22,7 +37,7 @@ function App() {
     <div className="App">
       <Canvas settings={settings} updateCount={updateCount}/>
       <Search />
-      <Settings />
+      <Settings settings={settings} handleSettingsChange={handleSettingsChange} handleUpdate={handleUpdate}/>
     </div>
   );
 }
--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -12,8 +12,45 @@ function Settings(props) {
   return (
     <div>
       <button className="button openButton" style={{top: window.innerHeight-60, left: window.innerWidth-60}} onClick={handleButtonClick}>&#9998;</button>
-      <div className="settingsMenu" style={{width: showing ? "250px" : "0"}}>
+      <div className="settingsMenu" style={{width: showing ? "300px" : "0"}}>
         <button className="button closeButton" onClick={handleButtonClick}>&times;</button>
+        <label className="title">
+            Graphics
+        </label>
+        <div className="variable">
+          <span className="variableName">
+            Width
+          </span>
+          <span className="variableInput">
+            <input type="range" id="width" name="width" min="10" max="1000" value={props.settings.width} onChange={props.handleSettingsChange} />
+          </span>
+          <span className="variableValue">
+            {props.settings.width}
+          </span>
+        </div>
+        <div className="variable">
+          <span className="variableName">
+            Height
+          </span>
+          <span className="variableInput">
+            <input type="range" id="trees" name="trees" min="10" max="1000" value={props.settings.height} onChange={props.handleSettingsChange} />
+          </span>
+          <span className="variableValue">
+            {props.settings.height}
+          </span>
+        </div>
+        <div className="variable">
+          <span className="variableName">
+            Trees
+          </span>
+          <span className="variableInput">
+            <input type="range" id="trees" name="trees" min="1" max="1000" value={props.settings.trees} onChange={props.handleSettingsChange} />
+          </span>
+          <span className="variableValue">
+            {props.settings.trees}
+          </span>
+        </div>
+        <button className="update" onClick={props.handleUpdate}>Update</button>
       </div>
     </div>
   );
